6237afee8a0de7dbdc1dbf2b6a8907c45de36652
Tai Nguyen-Trung <taint@pc-317.local.elarion.com>
2017-12-19 16:46:55 +0700

    Complete Button Search and Search picture by name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218083351) do
+ActiveRecord::Schema.define(version: 20171213075630) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -42,14 +42,6 @@ ActiveRecord::Schema.define(version: 20171218083351) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
-  create_table "searches", force: :cascade do |t|
-    t.string "keywords"
-    t.string "new"
-    t.string "show"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
